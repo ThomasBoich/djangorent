1082f10ab56ef742893c90dc858deb960f9195b1
ARTEM BOYKO <40269799+ThomasBoich@users.noreply.github.com>
2023-06-02 11:28:50 +0700

    add new_task page

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import widgets
 
 from .models import TaskComment, Task
 
@@ -13,6 +14,8 @@ class TaskCommentForm(forms.ModelForm):
 
 class addTaskForm(forms.ModelForm):
     #photo = MultiImageField(max_file_size=1024 * 1024 * 5, required=False)
+    date_start = forms.DateTimeField(widget=widgets.DateTimeInput(attrs={'type': 'datetime-local'}))
+    date_finish = forms.DateTimeField(widget=widgets.DateTimeInput(attrs={'type': 'datetime-local'}))
 
     class Meta:
         model = Task
